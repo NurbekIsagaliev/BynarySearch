825eadb82590681763e9d844d0287e8d080a66f0
NurbekIsagaliev <71828298+NurbekIsagaliev@users.noreply.github.com>
2022-02-25 17:36:27 +0500

    Update binary_search.php

--- binary_search.php
+++ binary_search.php
@@ -3,7 +3,13 @@
  search(array $data, int $number) : int которая вернет индекс элемента массива с заданным 
  значением или -1 в случае отсутствия данного значения в массиве. ВНИМАНИЕ: задача на 
  построение алгоритма, использование array_search и иже с ним не принимается. 
- Решение методом полного перебора так же не засчитывается*/
+ Решение методом полного перебора так же не засчитывается
+ Binary search. There is an array of integers. It is known a priori that the array is sorted
+ in ascending order of the values ​​of its elements. Need to write a function
+ search(array $data, int $number) : int which will return the index of the array element with the given
+ value or -1 if the given value is not present in the array. ATTENTION: task on
+ building an algorithm, using array_search and others like it is not accepted.
+ The brute-force solution does not count either.*/
 $data=[1,2,3,4,5,6,7,8,9,10];
 $number=4;
 function search(&$data, $number){
